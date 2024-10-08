d7185b4befdbeb97eea3985e79b3a9a2fcdce499
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-08 14:51:32 +0800

    Add my solution to 02-values-in-object

--- pojo_project/problems/02-values-in-object.js
+++ pojo_project/problems/02-values-in-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 
 Do this once using using a `for...in` loop and once using `Object.values`.
@@ -14,8 +14,18 @@ valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
 valuesInObject(foods); // => ["tart", "sour", "sweet"]
 ***********************************************************************/
 
+// function valuesInObject(obj) {
+// 	return Object.values(obj);
+// }
+
 function valuesInObject(obj) {
-  // Your code here
+	let values = [];
+
+	for (let key in obj) {
+		values.push(obj[key]);
+	}
+
+	return values;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
