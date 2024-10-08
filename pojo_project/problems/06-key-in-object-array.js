a9f6b833329665bc266ff5e93c5c434ccd57779c
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-08 15:10:14 +0800

    Add my solution to 06-key-in-object-array

--- pojo_project/problems/06-key-in-object-array.js
+++ pojo_project/problems/06-key-in-object-array.js
@@ -1,7 +1,7 @@
 /***********************************************************************
-Write a function `keyInObjectArray(objArray, keyString)` that takes in an array of 
-objects as the first parameter and a string as the second. The `keyInObjectArray` 
-will return `true` if any of the objects contains the `keyString` as a key within them, and 
+Write a function `keyInObjectArray(objArray, keyString)` that takes in an array of
+objects as the first parameter and a string as the second. The `keyInObjectArray`
+will return `true` if any of the objects contains the `keyString` as a key within them, and
 `false` if not.
 
 
@@ -20,7 +20,15 @@ keyInObjectArray(objArray, 'animal'); // => false
 ***********************************************************************/
 
 function keyInObjectArray(objArray, keyString) {
-  // Your code here
+	let keyExists = false;
+
+	objArray.forEach(function (obj) {
+		if (keyString in obj) {
+			keyExists = true;
+		}
+	});
+
+	return keyExists;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
